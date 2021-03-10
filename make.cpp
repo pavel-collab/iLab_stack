a7fd0cc6327d234255696f73baf91b1725a949bb
pavel-collab <rubikpf2002@mail.ru>
2021-03-10 19:56:44 +0300

    develop

--- make.cpp
+++ make.cpp
@@ -12,22 +12,24 @@ int main(int argc, char* argv[]) {
     else if (!strcmp(argv[1], "protected")) {
 
         system("gcc -c -D DEBUG_MODE lib.c");
-        system("gcc -c main.c\n");
+        system("gcc -c main.c");
         system("gcc -o main main.o lib.o\n");
-        //system("./main");
 
     }
     else if (!strcmp(argv[1], "no_protected")) {
 
-        system("gcc -c stack_f.c\n");
-        system("gcc -c main.c\n");
-        system("gcc -o main main.o stack_f.o\n");
-        //system("./main");
+        system("gcc -c lib.c");
+        system("gcc -c main.c");
+        system("gcc -o main main.o lib.o");
 
     }
     else {
         printf("ERROR, expected another argument\n");
         return -1;
     }
+
+    system("main.exe");
+
+    return 0;
 
 }
